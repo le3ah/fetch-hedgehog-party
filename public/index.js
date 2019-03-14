e070577bf45f078da42ed0757afe8192498dfbd1
Leah Miller <lkmiller.87@gmail.com>
2019-03-14 13:20:58 -0600

    Adds hedgehog party

--- public/index.js
+++ public/index.js
@@ -2,9 +2,18 @@ const getHedgehogs = () => {
   $('#hedgehog-info').html('');
 
   fetch(`https://hedgehog-party.herokuapp.com/api/v1/invites`)
+  //fetch request takes two arguments, the first is required and in the above example,
+  // is a URL (can also be an API endpoint).  The second is not required, but could
+  // include an object of configuration settings for the request
     .then(response => response.json())
+    /* .then() is the first of two methods we receive from the promise; once the response object
+    is available, the first .then() block will fire and in this instance converts the body into
+    a JSON data structure and returns another Promise, which is why we can chain another .then()*/
     .then(hedgehogs => appendHedgehogs(hedgehogs))
+    /* adding this second .then() is promise chaining. We are calling the appendHedgehogs method
+    from below, which takes hedgehogs as an argument */
     .catch(error => console.error({ error }));
+    /* if the request block fails, the .catch() block will run and we will log the error to the console*/
 };
 
 const appendHedgehogs = (hedgehogs) => {
@@ -30,10 +39,28 @@ const appendHedgehog = (hedgehog) => {
 };
 
 const addNewHedgehog = () => {
-  console.log("we are in the addNewHedgehog function");
+  event.preventDefault();
+  fetch(`https://hedgehog-party.herokuapp.com/api/v1/invites`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      "name": $("#name").val(),
+      "hoglets": $("#hoglets").val(),
+      "allergies": $("#allergies").val()
+    })
+  })
+  .then((response) => response.json())
+  .catch((error) => console.error({error}))
+  console.log("we are in the addNewHedgehog function")
 };
 
 const unInviteHedgehog = () => {
+  event.preventDefault();
+  var id = event.target.id
+  fetch(`https://hedgehog-party.herokuapp.com/api/v1/invites/${id}`, {
+    method: 'DELETE',
+    headers: { 'Access-Control-Allow-Methods': "DELETE" },
+    })
   console.log("we are in the unInviteHedgehog function");
 };
 
